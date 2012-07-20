568a75f221c8b5135c69e04b93a8b26d0129f547
Kamran <abc@gmail.com>
2012-07-20 15:08:30 +0500

    Revert "commit added at bottom of About.aspx"
    This reverts commit c165242e38e2345e1ca7521f2aac2163a5fafc1a.

--- AboutBox1.vb
+++ AboutBox1.vb
@@ -22,8 +22,5 @@
     Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
         Me.Close()
     End Sub
-private Sub BindIT()
-{
-]
 
 End Class
